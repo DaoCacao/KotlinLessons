f8b6cf8ed96cd862bbe59ff6692a619fc2c0ce0e
daocacao <mrdaocacao@gmail.com>
2023-02-19 15:26:48 +0400

    add lesson 3 homework

---
+++ src/main/kotlin/lesson_3/homework/Labdas.kt
@@ -0,0 +1,25 @@
+package lesson_3.homework
+
+/**
+ * Lambdas.
+ *
+ * Make your own implementations of kotlin list functions:
+ * - [filter]
+ * - [map]
+ * - [find]
+ * - [forEach]
+ *
+ * each function should have two parameters - list to process and lambda to apply. function should return result of processing.
+ *
+ * Example:
+ * - filter(listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)) { it % 2 == 0 } // should return listOf(2, 4, 6, 8, 10)
+ * - map(listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)) { it * 2 } // should return listOf(2, 4, 6, 8, 10, 12, 14, 16, 18, 20)
+ *
+ * Hint: read a little about those functions in the documentation.
+ *
+ * You can skip part with generics (<T> stuff) and just use [Int] as type parameter of lists and lambdas.
+ * Hard level for self learning: Don't skip, make your functions generic!
+ */
+fun main() {
+    TODO("Implement me")
+}
